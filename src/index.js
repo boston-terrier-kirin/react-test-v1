ebe56c2fb3c31e846677e050839330e2b3526cdc
Kohei Matsumoto <kohei.matsumoto@gmail.com>
2021-07-18 13:06:41 +0900

    add redux settings

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 import App from 'components/App';
+import commentsReducer from './reducers/commentsReducer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(combineReducers({ comments: commentsReducer }));
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
---
+++ src/reducers/commentsReducer.js
@@ -0,0 +1,7 @@
+const initialState = [];
+
+const commentsReducer = (state = initialState, action) => {
+	return state;
+};
+
+export default commentsReducer;
