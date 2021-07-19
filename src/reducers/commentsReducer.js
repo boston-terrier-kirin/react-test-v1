140a78e1b59eb2d6bb00c563af600dbb126e1cd7
Kohei Matsumoto <kohei.matsumoto@gmail.com>
2021-07-19 09:36:30 +0900

    完了

--- src/reducers/commentsReducer.js
+++ src/reducers/commentsReducer.js
@@ -1,10 +1,14 @@
-import { SAVE_COMMENT } from 'actions/types';
+import { SAVE_COMMENT, FETCH_COMMETS } from 'actions/types';
 
 const initialState = [];
 
 const commentsReducer = (state = initialState, action) => {
 	if (action.type === SAVE_COMMENT) {
 		return [...state, action.payload];
+	}
+	if (action.type === FETCH_COMMETS) {
+		const comments = action.payload.data.map((comment) => comment.name);
+		return [...state, ...comments];
 	}
 
 	return state;
